a0b52bd09dfa08fe4868adb195175e890a8725e4
Sören Penter <soeren@jocoon.de>
2023-03-23 12:15:01 +0100

    Adjusted package.json

---
+++ demos/2.x/dist/js/chunk-2d22d0a1.a9549c57.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d0a1"],{f694:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" 403 Forbidden ")])},c=[],l=t("2877"),r={},a=Object(l["a"])(r,u,c,!1,null,null,null);e["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d22d0a1.a9549c57.js.map
---
+++ demos/2.x/dist/js/chunk-2d22dbb4.6dd36ffe.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22dbb4"],{f96a:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" 404 Not Found ")])},c=[],l=t("2877"),o={},a=Object(l["a"])(o,u,c,!1,null,null,null);e["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d22dbb4.6dd36ffe.js.map
